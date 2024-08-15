f216946e0fba0f516f0910218a92d8124e50ece3
jcrayb <jcrb7800@gmail.com>
2024-08-15 21:40:21 +0200

    mechrefify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  ssite: 'https://jcrayb.github.io',
+  site: 'https://jcrayb.github.io',
   base: 'mechref',
   integrations: [svelte()]
 });
