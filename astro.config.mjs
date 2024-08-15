a1c09c844a4068f369a8cd7c5d82885d86ed3964
Chris <jcrb7800@gmail.com>
2024-08-15 21:22:56 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://mechref.org',
+  ssite: 'https://jcrayb.github.io',
   integrations: [svelte()]
 });
