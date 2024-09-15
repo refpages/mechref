42d815ca66f47e941eb10f2fa1e2371825b93a11
jcrayb <jcrb7800@gmail.com>
2024-09-15 16:10:16 -0500

    moved some vars around, fixed sidebar btns

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -18,11 +18,6 @@
 </script>
 
 <style>
-    .sidebar-btn:before{
-        content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='rgba%280,0,0,.5%29' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
-    }
-
-
     .course-list{
         list-style-type: none;
     }
