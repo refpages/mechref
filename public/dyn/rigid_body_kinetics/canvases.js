840bd8bcae727d978bf5dadf77be13c3b921064a
agyao2 <agyao2@illinois.edu>
2025-07-30 23:08:01 -0400

    combining accelerating/braking and banked turns
    restores accelerating/braking page and banked turns page back to how the original site was

--- public/dyn/rigid_body_kinetics/canvases.js
+++ public/dyn/rigid_body_kinetics/canvases.js
@@ -1994,6 +1994,57 @@ $(document).ready(function() {
         this.restore();
     });
 
+    ava_fp_c = new PrairieDrawAnim("ava-fp-c", function(t) {
+        this.addOption("components", true);
+        
+        var stateTogether = {bodyOffset: 0};
+        var stateApart = {bodyOffset: 3};
+        var states = [stateTogether, stateApart];
+        var transTimes = [0.5, 0.5];
+        var holdTimes = [-1, -1];
+        var interps = {};
+        var names = ["together", "apart"];
+        var fbdState = this.newSequence("fbd", states, transTimes, holdTimes, interps, names, t);
+        var drawForces = false;
+        if ((fbdState.inTransition == false) && (fbdState.index == 1)) {
+            drawForces = true;
+        }
+        
+        var motionState = Car.getMotion(this, t);
+        
+	this.setUnits(16, 3.5 + fbdState.bodyOffset, 600);
+        var desiredPos = this.pos2Dw($V([0, this.heightPx() - this.getProp("groundDepthPx") * 3]));
+        this.translate($V([-Car.centerOfMass.e(1), desiredPos.e(2)]));
+
+        var fAir = Car.fAir(motionState.v)
+        var fDrive = fAir + Car.mass * motionState.a;
+        var fg = Car.mass * 9.81;
+        
+        var groundOffset = - motionState.x * Car.motionScale;
+        var centerContact = $V([Car.centerOfMass.e(1), Car.rearContact.e(2)]);
+        
+        // body and wheels
+        this.save();
+        this.translate($V([0, fbdState.bodyOffset]));
+        Car.drawBody(this);
+        Car.drawWheels(this, 0);
+        if (drawForces) {
+            this.centerOfMass(Car.centerOfMass);
+            Car.drawForce(this, Car.centerOfMass, $V([0, -fg]));
+            Car.drawForce(this, Car.windContact, $V([-fAir, 0]));
+            Car.drawForce(this, centerContact, $V([fDrive, fg]));
+            this.arrowFrom(Car.centerOfMass, $V([motionState.a, 0]).x(Car.motionScale), "acceleration");
+            this.arrowFrom(Car.centerOfMass, $V([motionState.v, 0]).x(Car.motionScale), "velocity");
+        }
+        this.restore();
+        
+        // ground
+        this.groundHashed($V([0, 0]), $V([0, 1]), 25, groundOffset);
+        if (drawForces) {
+            Car.drawForce(this, centerContact, $V([-fDrive, -fg]));
+        }
+    });
+
     ava_fr_c = new PrairieDrawAnim("ava-fr-c", function(t) {
         this.addOption("components", true);
         
@@ -2183,6 +2234,127 @@ $(document).ready(function() {
         this.measurement(Q3, Q2, "TEX:$h_3$");
     });
 
+    avb_fg_c = new PrairieDrawAnim("avb-fg-c", function(t) {
+        this.addOption("vectors", false);
+        
+	this.setUnits(11, 11 / this.goldenRatio);
+
+        var d = 4;
+        var h = 4;
+        var w = 1;
+
+        this.rod($V([-d/2, 0]), $V([d/2, 0]), h);
+        this.rod($V([-d/2, 0]), $V([d/2, 0]), h - 2 * w);
+
+        var r = (h - w) / 2; // radius of track center
+        var l1 = d; // top horizontal center length
+        var l2 = Math.PI * r; // right curve center length
+        var l3 = d; // bottom horizontal center length
+        var l4 = Math.PI * r; // left curve center length
+        var l = l1 + l2 + l3 + l4; // total length of track center
+        var v = 2; // velocity of vehicle
+
+        var computePos = function(t) {
+            var dataNow = {};
+            // dataNow.P = position of vechicle
+            // dataNow.theta = angle of normal
+            var s = (t * v) % l; // distance along track
+            if (s < l1) {
+                // top horizontal
+                dataNow.P = $V([-d/2 + s, r]);
+                dataNow.theta = Math.PI / 2;
+            } else if (s < l1 + l2) {
+                // right curve
+                dataNow.theta = Math.PI/2 - (s - l1) / r;
+                dataNow.P = $V([d/2 + r * Math.cos(dataNow.theta), r * Math.sin(dataNow.theta)]);
+            } else if (s < l1 + l2 + l3) {
+                // bottom horizontal
+                dataNow.P = $V([d/2 - (s - l1 - l2), -r]);
+                dataNow.theta = -Math.PI/2;
+            } else {
+                // left curve
+                dataNow.theta = -Math.PI/2 - (s - l1 - l2 - l3) / r;
+                dataNow.P = $V([-d/2 + r * Math.cos(dataNow.theta), r * Math.sin(dataNow.theta)]);
+            }
+            return dataNow;
+        }
+        var data = this.numDiff(computePos, t);
+
+        this.save();
+        this.translate(data.P);
+        this.rotate(data.theta - Math.PI/2);
+        this.rectangle(0.4, 0.2);
+        this.restore();
+
+        if (this.getOption("vectors")) {
+            this.arrowFrom(data.P, data.diff.P.x(0.4), "velocity");
+            this.labelLine(data.P, data.P.add(data.diff.P.x(0.4)), $V([1, 0]), "TEX:$\\vec{v}$");
+            if (data.ddiff.P.modulus() > 1e-5) {
+                this.arrowFrom(data.P, data.ddiff.P.x(0.4), "acceleration");
+                this.labelLine(data.P, data.P.add(data.ddiff.P.x(0.4)), $V([1, 0]), "TEX:$\\vec{a}$");
+                this.arrowTo(data.P, data.ddiff.P.x(0.6), "force");
+                this.labelLine(data.P, data.P.add(data.ddiff.P.x(-0.6)), $V([1, 0]), "TEX:$\\vec{F}$");
+            }
+        }
+    });
+
+    avb_fp_c = new PrairieDrawAnim("avb-fp-c", function(t) {
+
+        this.addOption("thetaDeg", 45);
+        this.addOption("velocity", 30);
+        this.addOption("components", true);
+
+	this.setUnits(8, 8 / this.goldenRatio);
+
+        var theta = this.getOption("thetaDeg") / 180 * Math.PI;
+
+        fbdTrans = this.activationSequence("fbd", 0.5, t);
+        fbdOffset = fbdTrans * (2 + 2 * Math.sin(theta));
+        var drawForces = (fbdTrans == 1);
+        
+        var velocity = this.getOption("velocity"); // m/s
+        var acceleration = Math.pow(velocity, 2) / bus.radius; // m/s^2
+
+        var normal = bus.mass * acceleration * Math.sin(theta) + bus.mass * bus.gravity * Math.cos(theta);
+        var friction = - bus.mass * acceleration * Math.cos(theta) + bus.mass * bus.gravity * Math.sin(theta);
+
+        var u = this.vector2DAtAngle(theta);
+        var v = this.vector2DAtAngle(theta + Math.PI/2);
+
+        this.translate($V([fbdTrans * 0.5 * Math.sin(theta), -1 + fbdOffset / 4]));
+
+        // ground
+        this.save();
+        this.translate(v.x(-fbdOffset / 2));
+        this.groundHashed($V([0, 0]), v, 10, 0);
+        if (drawForces) {
+            bus.drawForce(this, $V([0, 0]), u.x(-friction), v.x(-normal));
+        }
+        this.restore();
+
+        // bus
+        this.save();
+        this.translate(v.x(fbdOffset / 2));
+        var busScale = 0.4;
+        var C = v.x(busScale * bus.centerOfMass.e(2));
+        this.save();
+        this.rotate(this.angleOf(u));
+        this.scale($V([busScale, busScale]));
+        bus.draw(this);
+        this.restore();
+        if (drawForces) {
+            this.centerOfMass(C);
+            if (Math.abs(acceleration) > 1e-5) {
+                this.arrowFrom(C, $V([-acceleration, 0]).x(bus.motionScale), "acceleration");
+                this.labelLine(C, C.add($V([-acceleration, 0]).x(bus.motionScale)), $V([1,0]), "TEX:$\\vec{a}$");
+            }
+            bus.drawForce(this, C, $V([0, -bus.mass * bus.gravity]));
+            bus.drawForce(this, $V([0, 0]), u.x(friction), v.x(normal));
+        }
+        this.restore();
+
+    });
+
     window.avb_fr_c = new PrairieDrawAnim("avb-fr-c", function(t) {
 
         this.addOption("thetaDeg", 45);
@@ -2309,9 +2481,12 @@ $(document).ready(function() {
         rem_eh_c.redraw();
         rem_eb_c.redraw();
         rem_ew_c.redraw();
+        ava_fp_c.redraw();
         ava_fr_c.redraw();
         ava_fm_c.redraw();
         ava_fc_c.redraw();
+        avb_fg_c.redraw();
+        avb_fp_c.redraw();
         avb_fr_c.redraw();
         avb_fb_c.redraw();
     });
