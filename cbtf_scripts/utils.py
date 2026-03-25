fe01c42f30fd0145fbf247c9974236c58565ee32
Emily Tam <learnemt007@gmail.com>
2026-03-25 10:42:44 -0500

    MathJax and Offline CBTF Script Change (#446)
    MathJax and Offline CBTF  Script Change

--- cbtf_scripts/utils.py
+++ cbtf_scripts/utils.py
@@ -1,41 +1,53 @@
 import shutil
 import os
+import re
 from pathlib import PurePath
 
+def fix_canvases_js_paths(home, courses):
+    for course in courses:
+        course_dir = os.path.join(home, course)
+        if not os.path.exists(course_dir):
+            continue
+        for item in os.listdir(course_dir):
+            js_path = os.path.join(course_dir, item, 'canvases.js')
+            if os.path.exists(js_path):
+                with open(js_path, 'r', encoding='utf8') as f:
+                    data = f.read()
+                # rewrite /course/page/image.ext -> page/image.ext
+                data = re.sub(r'"/' + course + r'/([^/]+)/([^"]+\.(jpg|jpeg|png|gif|svg|webp|mp4|csv|json))"', r'"\1/\2"', data)
+                # rewrite bare image filenames -> page/image.ext
+                data = re.sub(r'"([^"/]+\.(jpg|jpeg|png|gif|svg|webp|mp4|csv|json))"', rf'"{item}/\1"', data)
+                with open(js_path, 'w', encoding='utf8') as f:
+                    f.write(data)
+
+# Files that must stay in their page subfolders because they have hardcoded paths to assets in the same folder
+STAY_IN_SUBFOLDER = {'canvases.js', 'py_triples.js', 'worldCoastlineCompressed.js'}
+
 def explore_dir_and_move(home, dir, banned_dirs):
-    #print(os.path.join(home, dir))
     dirs = [d for d in os.listdir(os.path.join(home, dir)) if (os.path.isdir(os.path.join(os.path.join(home, dir), d)) and d not in banned_dirs)]
     htmls = [d for d in os.listdir(os.path.join(home, dir)) if (d == 'index.html')]
     js = [d for d in os.listdir(os.path.join(home, dir)) if (d[-3:] == '.js')]
-    #print(dirs, htmls, js)
+
     if dir != '':
         for h in htmls:
             try:
-                
                 ppth = PurePath(os.path.join(os.path.join(home, dir), h))
                 file_name = ppth.parts[-2]+'.html'
-                #print(os.path.join("/".join(ppth.parts[:-2]), file_name))
                 shutil.move(os.path.join(os.path.join(home, dir), h), os.path.join("/".join(ppth.parts[:-2]), file_name))
             except Exception as e:
                 print(e)
                 pass
 
         for j in js:
+            if j in STAY_IN_SUBFOLDER:
+                continue  
             try:
-                
                 ppth = PurePath(os.path.join(os.path.join(home, dir), j))
-                file_name = ppth.parts[-2]+'.js'
-                
-                if j == 'canvases.js':
-                    #print(os.path.join("/".join(ppth.parts[:-2]), file_name))
-                    shutil.move(os.path.join(os.path.join(home, dir), j), os.path.join("/".join(ppth.parts[:-2]), file_name))
-                else:
-                    #print(os.path.join("/".join(ppth.parts[:-2]), j))
-                    shutil.move(os.path.join(os.path.join(home, dir), j), os.path.join("/".join(ppth.parts[:-2]), j))
+                shutil.move(os.path.join(os.path.join(home, dir), j), os.path.join("/".join(ppth.parts[:-2]), j))
             except Exception as e:
                 print(e)
                 pass
-        
+
     for d in dirs:
         explore_dir_and_move(home, os.path.join(dir, d), banned_dirs)
     return dirs
